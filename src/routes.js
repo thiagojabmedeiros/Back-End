28848fb09a53bebe362c61f284814a8d002ef801
Thiago Medeiros <thiagojabm@gmail.com>
2026-03-18 18:13:19 -0300

    studying

---
+++ src/routes.js
@@ -0,0 +1,23 @@
+export const routes = [
+    {
+        method: "GET",
+        path: "/products",
+        controller: (request, response) =>{
+            return response.end("List of products.")
+        }
+    },
+    {
+        method: "POST",
+        path: "/products",
+        controller: (request, response) => {
+            return response.end(JSON.stringify(request.body))
+        }
+    },
+    {
+        method: "DELETE",
+        path: "/products/:id",
+        controller: (request, response) => {
+            return response.end("The product was removed.")
+        }
+    }
+]
--- src/server.js
+++ src/server.js
@@ -1,21 +1,10 @@
 import http from "node:http"
 import { jsonBodyHandler } from "./middlewares/jsonBodyHandler.js"
+import { routeHandler } from "./middlewares/routeHandler.js"
 
 const server = http.createServer(async (request, response) => {
-    const { method, url } = request
     await jsonBodyHandler(request, response)
-
-    if (method === "GET" && url === "/products") {
-        return response.writeHead(200).end("List of products!")
-    } 
-    else if (method === "POST" && url === "/products") {
-        console.log(request.body)
-        return response.writeHead(201).end(JSON.stringify(request.body))
-    } 
-    else {
-        return response.writeHead(404).end("Not Found.")
-    }
-
+    routeHandler(request, response)
 })
 
 server.listen(3333)
