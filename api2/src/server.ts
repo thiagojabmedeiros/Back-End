5e2281b0368040c44cdad1e990848c51eeab8e6e
Thiago Medeiros <thiagojabm@gmail.com>
2026-03-24 19:05:57 -0300

    api rest

---
+++ api2/src/server.ts
@@ -0,0 +1,8 @@
+function sum(a: number, b: number): number {
+    return a + b
+}
+
+const result: number = sum(5, 4)
+const name: String = "thiago medeiros"
+console.log(name)
+console.log("Sum result: ", result)
