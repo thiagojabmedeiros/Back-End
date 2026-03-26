2988ad9eea970352ab8eb6f097bd6f3ba085517b
Thiago Medeiros <thiagojabm@gmail.com>
2026-03-26 17:33:50 -0300

    .

--- api2/src/controllers/ProductsController.ts
+++ api2/src/controllers/ProductsController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express"
 import { AppError } from "../utils/AppError"
+import { z } from "zod"
+
 export class ProductsController {
     /**
      * index - GET
@@ -14,10 +16,20 @@ export class ProductsController {
         response.send(`page ${page} of ${limit}.`)
     }
     create(request: Request, response: Response) {
-        const {name, price} = request.body
-        if (!name || !price) {
-            throw new AppError("this creation needs name", 404)
-        }
+        const bodySchema = z.object({
+            name: z
+            .string({required_error: "name is required"})
+            .trim()
+            .min(6, {message: "name must be 6 or more characters"}),
+            price: z
+            .number({required_error: "price is required"})
+            .positive({message: "price must be positive"})
+            .gte(10)
+        })
+        const { name, price } = bodySchema.parse(request.body)
+        // if (!name || !price) {
+        //     throw new AppError("this creation needs name", 404)
+        // }
         // throw new Error("Error to create product")
         // throw new AppError("Error new")
 
--- api2/src/server.ts
+++ api2/src/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction} from "express"
 import { routes } from "./routes"
 import { AppError } from "./utils/AppError"
+import { ZodError } from "zod"
 
 const PORT = 3333
 
@@ -12,6 +13,9 @@ app.use(routes)
 app.use((error: any, request: Request, response: Response, next: NextFunction) => {
     if (error instanceof AppError) {
         return response.status(error.statusCode).json({message: error.message})
+    }
+    if (error instanceof ZodError) {
+        return response.status(400).json({message: "validation error", issues: error.format()})
     }
     response.status(500).json({ message: error.message })
 })
