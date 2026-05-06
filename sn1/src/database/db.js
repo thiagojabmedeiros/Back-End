c8cee21de2e9a72d331248260a97e3132d4b178f
Thiago Medeiros <thiagojabm@gmail.com>
2026-05-06 12:35:35 -0300

    creating api structure

--- sn1/src/database/db.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize')
-const config = require('../config/config')
-const User = require('../models/User')
-
-const database = new Sequelize(config.development)
-
-User.init(database)
-
-module.exports = database
--- sn1/src/routes/index-routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const userRoutes = require('./user-routes')
-const routes = express.Router()
-
-routes.use('/users', userRoutes)
-
-module.exports = routes
