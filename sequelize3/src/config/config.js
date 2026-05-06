7fa4cc46e8950fcb07b8b8fb903cd498fc5f273f
Thiago Medeiros <thiagojabm@gmail.com>
2026-05-06 13:04:19 -0300

    configs

---
+++ sequelize3/src/config/config.js
@@ -0,0 +1,35 @@
+const dotEnv = require('dotenv')
+dotEnv.config()
+
+const configs = {
+  "development": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PWD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT,
+    "port": Number(process.env.DB_PORT),
+    "logging": true,
+    "define": {
+      "timestamps": true
+    }
+  },
+  "test": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PWD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT,
+    "port": Number(process.env.DB_PORT),
+  },
+  "production": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PWD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT,
+    "port": Number(process.env.DB_PORT),
+  }
+}
+
+module.exports = configs
