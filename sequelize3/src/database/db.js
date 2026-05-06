6a2da9ac7e8acdbd258565ef42097c73b4cb0356
Thiago Medeiros <thiagojabm@gmail.com>
2026-05-06 13:23:46 -0300

    fixing error

--- sequelize3/src/database/db.js
+++ sequelize3/src/database/db.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize')
 const configs = require('../config/config')
 const User = require('../models/User')
 
-const database = new Sequelize(configs)
+const database = new Sequelize(configs.development)
 
 User.init(database)
 
--- sequelize3/src/server.js
+++ sequelize3/src/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = 3333
 const routes = require('./routes/index-routes')
+const database = require('./database/db')
 
 app.use(express.json())
 app.use(routes)
