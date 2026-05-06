4b627daded33be6702b71c494168a582ad66e196
Thiago Medeiros <thiagojabm@gmail.com>
2026-05-06 13:22:24 -0300

    database configs + model connection added

---
+++ sequelize3/src/database/db.js
@@ -0,0 +1,9 @@
+const Sequelize = require('sequelize')
+const configs = require('../config/config')
+const User = require('../models/User')
+
+const database = new Sequelize(configs)
+
+User.init(database)
+
+module.exports = database
