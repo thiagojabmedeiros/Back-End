63f517202c002d8cdd5618ea9cfa971c678d9fba
Thiago Medeiros <thiagojabm@gmail.com>
2026-05-12 18:29:53 -0300

    second migration

--- apiTest2/src/controllers/UserController.js
+++ apiTest2/src/controllers/UserController.js
@@ -1,3 +1,4 @@
+
 class UserRoutes {
     async create(request, response) {
 
--- apiTest2/src/migrations/20260512211605-create_users_table.js
+++ apiTest2/src/migrations/20260512211605-create_users_table.js
@@ -3,20 +3,21 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      }
+    })
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('users')
   }
 };
