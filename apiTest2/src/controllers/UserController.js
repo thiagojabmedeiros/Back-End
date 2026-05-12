ac57b69d613b9548d57fa15d2a9456549d014180
Thiago Medeiros <thiagojabm@gmail.com>
2026-05-12 18:22:31 -0300

    creating controller structure

---
+++ apiTest2/src/controllers/UserController.js
@@ -0,0 +1,13 @@
+class UserRoutes {
+    async create(request, response) {
+
+    }
+    async search(request, response) {
+
+    }
+    async index(request, response) {
+
+    }
+}
+
+module.exports = UserRoutes
--- apiTest2/src/routes/user-routes.js
+++ apiTest2/src/routes/user-routes.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const userRoute = express.Router()
+const UserController = require('../controllers/UserController')
+const userController = new UserController()
 
-userRoute.get('/', )
+userRoute.get('/', userController.index)
+userRoute.get('/:id', userController.search)
+userRoute.post('/', userController.create)
 
 module.exports = userRoute
