cc5ab9bd2f1ab565a9670659c4fea57bf33b051f
Thiago Medeiros <thiagojabm@gmail.com>
2026-05-12 18:46:35 -0300

    third migration

--- apiTest2/src/controllers/UserController.js
+++ apiTest2/src/controllers/UserController.js
@@ -1,7 +1,10 @@
+const User = require('../models/User')
 
 class UserRoutes {
     async create(request, response) {
-
+        const { name, email } = request.body
+        const user = await User.create({ name, email })
+        return response.status(201).json({ name, email })
     }
     async search(request, response) {
 
