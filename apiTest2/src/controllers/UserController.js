7e40bcf03976cf5820397065e25934489173af10
Thiago Medeiros <thiagojabm@gmail.com>
2026-05-12 19:02:48 -0300

    zod validation + creating user function

--- apiTest2/src/controllers/UserController.js
+++ apiTest2/src/controllers/UserController.js
@@ -1,14 +1,30 @@
 const User = require('../models/User')
+const zod = require('zod')
+const z = zod.z
 
 class UserRoutes {
+
     async create(request, response) {
-        const { name, email } = request.body
-        const user = await User.create({ name, email })
-        return response.status(201).json({ name, email })
+        try {
+            const bodySchema = z.object({
+                name: z.string({ message: "name required." }).trim().min(3, { message: "at least three characthers." }),
+                email: z.string({ message: "email required." }).email({ message: "invalid email format." }).trim()
+            })
+
+            const { name, email } = bodySchema.parse(request.body)
+
+            const user = await User.create({ name, email })
+
+            return response.status(201).json({ name, email })
+        } catch(error) {
+            return response.status(400).json({ message: error })
+        }
     }
+
     async search(request, response) {
 
     }
+
     async index(request, response) {
 
     }
