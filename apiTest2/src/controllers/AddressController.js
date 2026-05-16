a2ad349b061f65c33984ffe3cd01a5f391a0d749
Thiago Medeiros <thiagojabm@gmail.com>
2026-05-16 15:57:02 -0300

    address controller functions

--- apiTest2/src/controllers/AddressController.js
+++ apiTest2/src/controllers/AddressController.js
@@ -42,7 +42,37 @@ class AddressController {
         
     }
     async search(request, response) {
+        try {
+            const idSchema = z.object({
+                userId: z
+                .coerce
+                .number({ required_error: 'id is required' })
+                .int({ message: 'id is an integer'})
+            })
+            const { userId } = idSchema.parse(request.params)
+            const user = await User.findByPk(userId)
 
+            if (!user) {
+                return response.status(400).json({ message: 'user does not exist '})
+            }
+            const addresses = await Address.findAll({
+                where: { 
+                    userId: userId
+                }
+            })
+
+            if (addresses.length === 0) {
+                return response.json({ message: "this user has no addresses associated" })
+            }
+
+            return response.status(200).json(addresses)
+        } catch(error) {
+            if (error instanceof z.ZodError) {
+                return response.status(400).json({ message: error.issues })
+            }
+
+            return response.status(500).json({ message: error })
+        }
     }
     async index(request, response) {
 
--- apiTest2/src/routes/address-routes.js
+++ apiTest2/src/routes/address-routes.js
@@ -4,7 +4,7 @@ const AddressController = require('../controllers/AddressController')
 const addressController = new AddressController()
 
 addressRoutes.get('/addresses', addressController.index)
-addressRoutes.get('/:userId/address', addressController.search)
+addressRoutes.get('/:userId/addresses', addressController.search)
 addressRoutes.post('/:userId/address', addressController.create)
 addressRoutes.patch('/:userId/adresses/:id', addressController.change)
 addressRoutes.delete('/:userId/adresses/:id', addressController.remove)
